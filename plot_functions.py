6acab98004d5d019395b68f5ede83d08ef189650
m-miller33 <msmiller3366@gmail.com>
2025-10-27 13:36:47 -0400

    moved functions into the new plot_functions file

---
+++ plot_functions.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def make_hist(count, n_bins):
+    plt.hist(count, bins = n_bins)
+    plt.title("My Histogram")
+    plt.show()
+
+data = [4,2,6,7,5,6,8,5,8,6]
+n_bins = 7
